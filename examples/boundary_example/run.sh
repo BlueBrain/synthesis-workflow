0ec159b970539087b874904859d2dc911091c6d2
arnaudon <alexis.arnaudon@epfl.ch>
2023-10-19 16:10:45 +0200

    add example

---
+++ examples/boundary_example/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+
+export OMP_NUM_THREADS=1
+export REGION_GROWER_BOUNDARY_DEBUG=1
+
+rm -rf atlas
+brainbuilder atlases -n 1 -t 100 -d 10 -o atlas column -a 200
+
+
+python -m luigi --module synthesis_workflow.tasks.workflows ValidateSynthesis \
+    --local-scheduler \
+    --log-level INFO \
